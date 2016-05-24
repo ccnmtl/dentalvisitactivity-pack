925bf1f78a0a0a68ac090b1e279beef860e7e512
Susan Dreher <sdreher@columbia.edu>
2016-05-24 16:09:51 -0400

    NumberStepView - replace Backbone Router
    Factoring out the Backbone router in favor of a more explicit wizard-like approach. This architecture more cleanly supports the disabled & next states without having to fight with the router's default behavior.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-/* global jQuery: true, Backbone: true */
+/* global jQuery: true */
 
-// load css
+// load and apply css
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../node_modules/bootstrap-arrow-buttons/dist/' +
+        'css/bootstrap-arrow-buttons.css');
 require('../static/css/common.css');
+require('../static/css/steps.css');
 require('../static/css/dentalvisit.css');
 
 var jQuery = require('jquery');
@@ -10,5 +13,4 @@ var module = require('./dentalvisit.js');
 
 jQuery(document).ready(function() {
     module.DentalVisitApp.initialize();
-    Backbone.history.start();
 });
