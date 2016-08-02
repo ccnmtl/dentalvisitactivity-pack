e136c2cae232a114d3800aaa5253b1e6b6051e28
Susan Dreher <sdreher@columbia.edu>
2016-08-02 11:49:38 -0400

    npm3: remove relative references to node_modules

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ require('!file?name=[name].[ext]!../static/index.html');
 require('!file?name=[name].[ext]!../static/img/ctl-logo.png');
 
 // load and apply css
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../node_modules/bootstrap-arrow-buttons/dist/' +
-        'css/bootstrap-arrow-buttons.css');
+require('!style!css!bootstrap/dist/css/bootstrap.min.css');
+require('!style!css!bootstrap-arrow-buttons/dist/css/' +
+        'bootstrap-arrow-buttons.css');
 require('../static/css/common.css');
 require('../static/css/steps.css');
 require('../static/css/dentalvisit.css');
